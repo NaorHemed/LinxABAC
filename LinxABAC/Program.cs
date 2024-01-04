b61cd476a7080c2e4eb139194e15678f8903bd46
naor hemed <naorhemed@gmail.com>
2024-01-04 14:59:59 +0200

    added docker compose support

--- LinxABAC/Program.cs
+++ LinxABAC/Program.cs
@@ -30,12 +30,8 @@ builder.Services.AddScoped<IUserAuthorizationService, UserAuthorizationService>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/attributes", (IRedisQueries redisQueries) =>
 {
